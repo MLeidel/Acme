4a59ca8d97ff9ac232c22f2c7112b9ad9e98842b
MLeidel <michaelleidel321@gmail.com>
2026-08-15 16:01:49 -0500

    about.html

--- ms_setup.sh
+++ ms_setup.sh
@@ -16,6 +16,8 @@ echo Begin installing needed packages
 echo ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 sudo apt update
+sudo apt update
+sudo apt install python3-gi python3-gi-cairo libgirepository1.0-dev gir1.2-gtk-3.0 gir1.2-webkit2-4.1
 sudo apt install python3-gi python3-gi-cairo libgirepository1.0-dev gir1.2-gtk-3.0 gir1.2-webkit2-4.1
 sudo apt install gstreamer1.0-plugins-base gstreamer1.0-plugins-good
 sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav
